b0de9983618c23f3076711eacff04273c46a6fb4
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-04-28 15:48:09 -0300

    Vip Car and Academy finalized

--- src/pages/Applications/Applications.jsx
+++ src/pages/Applications/Applications.jsx
@@ -1,5 +1,7 @@
 import "./applications.css";
 import logoVipCar from '../../assets/images/logos/goldenvipcar.png'
+import playstore from '../../assets/images/playstore.png'
+import applestore from '../../assets/images/applestore.png'
 import smartphone from '../../assets/images/smartphone.png'
 import { Topbar } from "../../components/Topbar/Topbar";
 import Navbar from "../../components/Navbar/Navbar";
@@ -17,18 +19,14 @@ function Aplications() {
             <h1>Conheça a Golden Vip Car</h1>
             <p>O Golden VIP CAR não é apenas um serviço de transporte, é o melhor aplicativo de mobilidade. Oferecemos suporte 100% eficiente com uma equipe sempre preparada e as menores taxas do mercado para o motorista e o passageiro. </p>
             </div>
-            <div className="buttonsAplications">
-            <a href="/">Acessar a Aplications</a>
-            </div>
-
             <div className="app">
                 <div className="text">
                     <h1>Vai de</h1>
                     <img src={logoVipCar} alt="" />
                     <p>Este aplicativo foi desenhado para quem busca um serviço de transporte executivo presente no próprio bairro e que garanta que você e sua família serão atendidos por um motorista conhecido com segurança.</p>
                     <div className="download">
-                    <img src={logoVipCar} alt="" />
-                    <img src={logoVipCar} alt="" />
+                    <img src={playstore} alt="" />
+                    <img src={applestore} alt="" />
                     </div>
                 </div>
                 <div className="imageApp">
@@ -36,16 +34,18 @@ function Aplications() {
                 </div>
             </div>
             <div className="appCar">
-            <div className="imageApp">
+            <div className="imageappCar">
                     <img src={smartphone} alt="smartphone" />
                 </div>
                 <div className="text">
                     <h1>Seja um Motorista</h1>
                     <img src={logoVipCar} alt="" />
-                    <p>Este aplicativo foi desenhado para quem busca um serviço de transporte executivo presente no próprio bairro e que garanta que você e sua família serão atendidos por um motorista conhecido com segurança.</p>
+                    <p>Alô Motorista 
+Você sabia que na Golden Vip Car você paga somente R$1 por corrida finalizada ? 
+
+Com a menor taxa do mercado você motorista lucra muito mais com as suas corridas.</p>
                     <div className="download">
-                    <img src={logoVipCar} alt="" />
-                    <img src={logoVipCar} alt="" />
+                    <img src={playstore} alt="" />
                     </div>
                 </div>
    
